074d26bbe4bf7a792404427787cd5eef3691d20a
luis-villasana <luisvillasana020@gmail.com>
2025-06-08 21:48:25 -0500

    added character count functionality and functionality for getting sorted list of dictionaries with character count

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_character_count, get_sorted_list
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -6,11 +6,20 @@ def get_book_text(filepath):
     
     return file_contents
 
+def print_report(path, word_count, sorted_list):
+    print("")
+    
+    
+    pass
+
 def main():
     frankenstein_book_path = "books/frankenstein.txt"
     frankenstein_book_text = get_book_text(frankenstein_book_path)
-
     frankenstein_word_count = get_num_words(frankenstein_book_text)
-    print(f"{frankenstein_word_count} words found in the document")
+    character_count = get_character_count(frankenstein_book_text)
+    
+    # print(f"{frankenstein_word_count} words found in the document")
+    # print(character_count)
+    # print(get_sorted_list(character_count))
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,29 @@ def get_num_words(book_text):
     # turn book_text argument into list and then get length of list
     word_count = len(book_text.split())
     return word_count
+
+def get_character_count(book_text):
+    character_count = {}
+    for c in book_text:
+        c_lower = c.lower()
+        if (c_lower in character_count):
+            character_count[c_lower] += 1
+        else:
+            character_count[c_lower] = 1
+    
+    return character_count
+
+def sort_on(dict):
+    return dict["num"]
+
+def get_sorted_list(dic):
+    list_of_dict = []
+    for key in dic:
+        if key.isalpha():
+            list_of_dict.append( {"char": key, "num": dic[key]} )
+    
+    list_of_dict.sort(reverse=True, key=sort_on)
+    return list_of_dict
+
+
+
