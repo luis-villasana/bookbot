6db94f4e6084f765fc9e16640c106f6857596ac7
luis-villasana <luisvillasana020@gmail.com>
2025-06-09 15:05:59 -0500

    added function print_report to print report

--- main.py
+++ main.py
@@ -7,19 +7,22 @@ def get_book_text(filepath):
     return file_contents
 
 def print_report(path, word_count, sorted_list):
-    print("")
-    
-    
-    pass
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for dic in sorted_list:
+        print(f"{dic["char"]}: {dic["num"]}")
+    print("============= END ===============")
 
 def main():
     frankenstein_book_path = "books/frankenstein.txt"
     frankenstein_book_text = get_book_text(frankenstein_book_path)
     frankenstein_word_count = get_num_words(frankenstein_book_text)
     character_count = get_character_count(frankenstein_book_text)
-    
-    # print(f"{frankenstein_word_count} words found in the document")
-    # print(character_count)
-    # print(get_sorted_list(character_count))
+    sorted_list = get_sorted_list(character_count)
+
+    print_report(frankenstein_book_path, frankenstein_word_count, sorted_list)
 
 main()
