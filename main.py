d7f78e7e13fa519908ce2c17efa3bac6cddf11ab
luis-villasana <luisvillasana020@gmail.com>
2025-06-08 19:27:10 -0500

    added functionality to get_num_words to return word_count, and modified main fn to print word count of frakenstein text

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 def get_num_words(book_text):
-    pass
+    # turn book_text argument into list and then get length of list
+    word_count = len(book_text.split())
+    return word_count
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -8,7 +10,10 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    frankenstein_book_path = "books/frankenstein.txt"
+    frankenstein_book_text = get_book_text(frankenstein_book_path)
 
+    frankenstein_word_count = get_num_words(frankenstein_book_text)
+    print(f"{frankenstein_word_count} words found in the document")
 
 main()
